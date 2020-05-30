5f815ca33c0093090e7f318a281f8c6f58d17f0d
Daniel Lopez <ext_delopezp@falabella.cl>
2020-05-30 15:07:58 -0400

    Operador Map de los Observables

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,8 +16,7 @@ export class HomeComponent implements OnInit {
   ) {
     this.spotifyService.getNewRelease()
       .subscribe((data: any) => {
-        console.log(data.albums.items);
-        this.nuevasCanciones = data.albums.items;
+        this.nuevasCanciones = data;
       });
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  token = 'BQB5Y7q4yNqgcdxy22-F0zogVjlYAx30cL9FqQe8bk-ULeDmb-E9lNOPNymE6DWmHgoe9ZNzfZIfbViMk40';
+  token: string = 'BQBJM5Usji5gXsRujoaN3YW3qQEhVCYhJm7Hba9goif5Df0C_xUVuotBigXUBBP_DveLAJj2Akn-KQ4FJTw';
+  headers: HttpHeaders;
+  url: string = 'https://api.spotify.com/v1/';
 
   constructor(
     private httpClient: HttpClient
-  ) { }
-
-  getNewRelease() {
-    const headers = new HttpHeaders({
+  ) {
+    this.headers = new HttpHeaders({
       Authorization: `Bearer ${this.token}`
     });
+  }
 
-    return this.httpClient.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  getNewRelease() {
+    return this.httpClient
+      .get(`${this.url}browse/new-releases`, {headers: this.headers})
+      .pipe(map(data => data['albums'].items));
   }
 
   getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.token}`
-    });
-
-    return this.httpClient.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
+    return this.httpClient
+      .get(`${this.url}search?q=${termino}&type=artist&limit=15`, {headers: this.headers})
+      .pipe(map(data => data['artists'].items));
   }
 }
