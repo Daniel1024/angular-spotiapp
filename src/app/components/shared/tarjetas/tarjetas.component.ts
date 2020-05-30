2bf59fde700491222ccda6d3d92e28ce546f373b
Daniel Lopez <ext_delopezp@falabella.cl>
2020-05-30 16:49:12 -0400

    Página del artista, nueva ruta, parámetro por url y servicio

--- src/app/components/shared/tarjetas/tarjetas.component.ts
+++ src/app/components/shared/tarjetas/tarjetas.component.ts
@@ -1,18 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tarjetas',
   templateUrl: './tarjetas.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class TarjetasComponent implements OnInit {
 
   @Input() items: any[] = [];
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  verArtista(item: any) {
+    let artistaId = item.id;
+    if (item.type == 'album') {
+      artistaId = item.artists[0].id;
+    }
+    this.router.navigate(['/artist', artistaId]);
   }
 
 }
